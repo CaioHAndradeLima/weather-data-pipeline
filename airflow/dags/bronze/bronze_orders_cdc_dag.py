abe437c22931f232012d5084d04802f61d0d7ddc
Caio Henrique Andrade Lima <caiohandradelima@Caios-MacBook-Air.local>
2026-01-26 17:32:26 -0300

    fix black issue ci

--- airflow/dags/bronze/bronze_orders_cdc_dag.py
+++ airflow/dags/bronze/bronze_orders_cdc_dag.py
@@ -7,12 +7,12 @@ from src.ingestion.postgres.cdc.orders_cdc_consumer import consume_and_write_ord
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="bronze_kafka_order_events",
-    start_date=datetime(2024, 1, 1),
-    schedule_interval="@hourly",
-    catchup=False,
-    max_active_runs=1,
-    tags=["bronze", "kafka", "cdc"],
+        dag_id="bronze_kafka_order_events",
+        start_date=datetime(2024, 1, 1),
+        schedule_interval="@hourly",
+        catchup=False,
+        max_active_runs=1,
+        tags=["bronze", "kafka", "cdc"],
 ) as dag:
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
